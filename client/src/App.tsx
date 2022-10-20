f896326f8e3ba788bd004e8ce1d29f84cefcf0c4
kheiron <kheiron.gunnarsson@gmail.com>
2022-10-20 18:29:14 +0200

    set up client and server side

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from '../public/images/logo.png'
+import './app.scss';
+
+ const App=()=>{
+    return (
+    <>
+        <img src={logo} alt="fireSpot"/>
+        <h4>loading application</h4>
+    </>
+    )
+}
+export default App;
