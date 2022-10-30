e5b4bf45692f5335fdcc5dcec92c3a039cfe8dc1
kheiron <kheiron.gunnarsson@gmail.com>
2022-10-30 22:31:17 +0100

    moved SearchBox content to UI component

--- client/src/components/ui/Ui.tsx
+++ client/src/components/ui/Ui.tsx
@@ -1,13 +1,32 @@
-import { useRef, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ui.scss';
+import './searchBox.scss';
 
-import SearchBox from '../searchBox/SearchBox';
+import { GiWitchFlight } from "react-icons/gi";
+import Autofill from '../autofill/Autofill';
 import Results from '../results/Results';
 
 const Ui = () => {
+
+    const direction: { to: string, from: string } = { to: "to", from: "from" };
+
     return (
         <div className="UI">
-            <SearchBox />
+            <div className="search-box">
+                <form>
+                    <div className="app-logo">
+                        <GiWitchFlight color="#030E6D" fontSize="2em"  />
+                        flightRoutes
+                    </div>
+                    <div className="autofill-component from">
+                        <Autofill direction={direction.from} />
+                    </div>
+                    <div className="autofill-component to">
+                        <Autofill direction={direction.to}  />
+                    </div>
+                    <button>Find route</button>
+                </form>
+            </div>
             <Results />
         </div>
     );
