0232389c42bc8f85d43ff64bed112d0054eeb612
kheiron <kheiron.gunnarsson@gmail.com>
2022-11-01 23:03:02 +0100

    added routes collection to connnectToDatabase function

--- server/src/mongodb.ts
+++ server/src/mongodb.ts
@@ -1,14 +1,19 @@
 import * as mongoDB from "mongodb";
 import * as dotenv from "dotenv";
 
-export const collections: { airports?: mongoDB.Collection } = {}
+export const collections: {
+    airports?: mongoDB.Collection
+    routes?: mongoDB.Collection
+} = {}
 
 export async function connectToDatabase () {
     dotenv.config();
     const client: mongoDB.MongoClient = new mongoDB.MongoClient(`${process.env.MONGODB_URI}`);
     await client.connect();
     const db: mongoDB.Db = client.db(`${process.env.DB_NAME}`);
-    const airportsCollection: mongoDB.Collection = db.collection(`${process.env.AIRPORTS_COLLECTION_NAME}`);
-    collections.airports = airportsCollection;
-    console.log(`Successfully connected to database: ${db.databaseName} and collection: ${airportsCollection.collectionName}`);
+    const airports: mongoDB.Collection = db.collection(`${process.env.AIRPORTS_COLLECTION_NAME}`);
+    const routes: mongoDB.Collection = db.collection(`${process.env.ROUTES_COLLECTION_NAME}`);
+    collections.airports = airports;
+    collections.routes = routes;
+    console.log(`Connect to ${db.databaseName} db and ${airports.collectionName}, ${routes.collectionName} collections`);
 }
