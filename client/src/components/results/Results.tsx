1ca28528251d4f857f40ad52e48c5901a8272a6b
kheiron <kheiron.gunnarsson@gmail.com>
2022-11-07 14:45:56 +0100

    added paddings mixin

--- client/src/components/results/Results.tsx
+++ client/src/components/results/Results.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import './results.scss';
-import type { Route } from '../../../../server/src/models/zod'
+import type { Route } from '../../../../server/src/models/zod';
+
+import { RiRouteFill } from "react-icons/ri";
+import { RiFlightTakeoffLine } from "react-icons/ri";
+import { MdFlight } from "react-icons/md";
 
 type Props = {
     routes: Route[] | undefined;
@@ -11,12 +15,14 @@ const Results = ( { routes } : Props ) => {
 
     return (
         <div className="results">
+        <div className="navigation">
+            <RiRouteFill fontSize="1.5em" />
+            <MdFlight fontSize="1.5em" />
+            <RiFlightTakeoffLine fontSize="1.5em" />
+        </div>
             {routes &&
                 (
-                <div className="navigation">
-                    <p>test</p>
-                    <p>test</p>
-                </div>
+                 'test'
                 )
             }
         </div>
